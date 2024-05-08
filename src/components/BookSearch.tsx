391efff8e5d26a92c7c88b81b2769987a232dd8e
Heine Günther <heine_gunther@hotmail.com>
2024-05-08 12:04:21 +0200

    Added loading spinner on search button

--- src/components/BookSearch.tsx
+++ src/components/BookSearch.tsx
@@ -11,6 +11,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Book, useStore } from "@/store";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export const BookSearch = () => {
   const { books, addBook } = useStore((state) => state);
@@ -62,92 +63,108 @@ export const BookSearch = () => {
   const endIndex = Math.min(startIndex + resultsPerPage - 1, totalResults);
 
   return (
-    <div className="p-4">
-      <div className="sm:max-w-xs">
-        <Input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          onKeyUp={handleKeyPress}
-          placeholder="Søk etter din neste bok!"
-        />
-      </div>
-      <Button onClick={() => searchBooks()} disabled={isLoading}>
-        {isLoading ? "Søker..." : "Søk"}
-      </Button>
-      <div className="mt-2">
-        {totalResults > 0 && (
-          <p className="text-sm">
-            Viser {startIndex} - {endIndex} av {totalResults} resultater
-          </p>
-        )}
-      </div>
-      <div className="mt-4 max-h-64 overflow-auto">
-        {query.length > 0 && results.length > 0 ? (
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="p-2">Tittel</TableHead>
-                <TableHead className="p-2">Forfatter</TableHead>
-                <TableHead className="p-2">Utgitt år</TableHead>
-                <TableHead className="p-2">Antall sider</TableHead>
-                <TableHead className="p-2"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {results.map((book, index) => (
-                <TableRow key={index}>
-                  <TableCell>{book.title}</TableCell>
-                  <TableCell>{book.author_name}</TableCell>
-                  <TableCell>{book.first_publish_year}</TableCell>
-                  <TableCell>{book.number_of_pages_median || "-"}</TableCell>
-                  <TableCell>
-                    <Button
-                      variant="link"
-                      onClick={() =>
-                        addBook({
-                          key: book.key,
-                          title: book.title,
-                          author_name: book.author_name,
-                          first_publish_year: book.first_publish_year,
-                          number_of_pages_median:
-                            book.number_of_pages_median || null,
-                          status: "backlog",
-                        })
-                      }
-                      disabled={books.some((b) => b.key === book.key)}
-                    >
-                      Legg til
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        ) : (
-          <div className="flex max-h-60 items-center justify-center p-16">
-            <p>Start your search!</p>
+    <div className="-m-1.5 overflow-x-auto">
+      <div className="sm:divide-y sm:divide-gray-200 sm:rounded-2xl sm:border sm:dark:divide-gray-700 sm:dark:border-gray-700">
+        <div className="flex flex-col items-center gap-3 px-4 py-3 sm:flex-row">
+          <div className="relative w-full sm:max-w-xs">
+            <Input
+              type="text"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={handleKeyPress}
+              placeholder="Søk etter din neste bok!"
+            />
           </div>
-        )}
-      </div>
-      <div className="mt-4 flex items-center justify-between">
-        <Button
-          variant="outline"
-          onClick={handlePreviousClick}
-          disabled={currentPage <= 1 || isLoading}
-        >
-          Tilbake
-        </Button>
-        <span>Side {currentPage}</span>
-        <Button
-          variant="outline"
-          onClick={handleNextClick}
-          disabled={
-            currentPage > Math.ceil(totalResults / resultsPerPage) || isLoading
-          }
-        >
-          Neste
-        </Button>
+          <Button
+            className="max-sm:w-full sm:max-w-xs"
+            onClick={() => searchBooks()}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <>
+                <AiOutlineLoading3Quarters className="mr-2 h-4 w-4 animate-spin" />{" "}
+                Søker...{" "}
+              </>
+            ) : (
+              "Søk"
+            )}
+          </Button>
+        </div>
+        {/* <div className="mt-2">
+          {totalResults > 0 && (
+            <p className="text-sm">
+              Viser {startIndex} - {endIndex} av {totalResults} resultater
+            </p>
+          )}
+        </div> */}
+        <div className="mt-4 max-h-64 overflow-auto">
+          {query.length > 0 && results.length > 0 ? (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="p-2">Tittel</TableHead>
+                  <TableHead className="p-2">Forfatter</TableHead>
+                  <TableHead className="p-2">Utgitt år</TableHead>
+                  <TableHead className="p-2">Antall sider</TableHead>
+                  <TableHead className="p-2"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {results.map((book, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{book.title}</TableCell>
+                    <TableCell>{book.author_name}</TableCell>
+                    <TableCell>{book.first_publish_year}</TableCell>
+                    <TableCell>{book.number_of_pages_median || "-"}</TableCell>
+                    <TableCell>
+                      <Button
+                        variant="link"
+                        onClick={() =>
+                          addBook({
+                            key: book.key,
+                            title: book.title,
+                            author_name: book.author_name,
+                            first_publish_year: book.first_publish_year,
+                            number_of_pages_median:
+                              book.number_of_pages_median || null,
+                            status: "backlog",
+                          })
+                        }
+                        disabled={books.some((b) => b.key === book.key)}
+                      >
+                        Legg til
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          ) : (
+            <div className="flex max-h-60 items-center justify-center p-16">
+              <p>Start your search!</p>
+            </div>
+          )}
+        </div>
+        <div className="mt-4 flex items-center justify-between">
+          <Button
+            variant="outline"
+            onClick={handlePreviousClick}
+            disabled={currentPage <= 1 || isLoading}
+          >
+            Tilbake
+          </Button>
+          <span>Side {currentPage}</span>
+          <Button
+            variant="outline"
+            onClick={handleNextClick}
+            disabled={
+              currentPage > Math.ceil(totalResults / resultsPerPage) ||
+              isLoading
+            }
+          >
+            Neste
+          </Button>
+        </div>
       </div>
     </div>
   );
